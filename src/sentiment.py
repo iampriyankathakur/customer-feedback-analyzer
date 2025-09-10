0e975a07ee9fd886791bff969e70a7c18a3d6fb7
Priyanka Thakur <55808489+iampriyankathakur@users.noreply.github.com>
2025-09-10 04:25:37 -0400

    Update sentiment.py

--- src/sentiment.py
+++ src/sentiment.py
@@ -1,1 +1,9 @@
+from textblob import TextBlob
 
+def get_sentiment(text: str) -> str:
+    polarity = TextBlob(text).sentiment.polarity
+    if polarity > 0.1:
+        return "positive"
+    elif polarity < -0.1:
+        return "negative"
+    return "neutral"
